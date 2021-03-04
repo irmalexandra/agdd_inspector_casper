279c86af3c3304495034f9d3edf81f40d7bee411
Loki Alexander Hopkins <lokih@ru.is>
2021-03-04 15:24:33 +0000

    Crouch and Jump implemented.

--- Inspector Casper/Assets/Scripts/PlayerMovement.cs
+++ Inspector Casper/Assets/Scripts/PlayerMovement.cs
@@ -9,16 +9,30 @@ public class PlayerMovement : MonoBehaviour
     public PlayerController controller;
     public float runSpeed = 40f;
     
-    private float horizontalMove = 0f;
-    
+    private float horizontalMove;
+    private bool jump;
+    private bool crouch;
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
 
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
     }
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
