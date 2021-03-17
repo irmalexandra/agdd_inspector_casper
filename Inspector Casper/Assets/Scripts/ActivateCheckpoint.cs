c636b36ce4f1f00c5c9f6d84f44632af3972f8c7
Ríkharður Friðgeirsson <rikhardur,fridgeirsson@gmail.com>
2021-03-17 00:23:53 +0000

    added check point sprites and lights

--- Inspector Casper/Assets/Scripts/ActivateCheckpoint.cs
+++ Inspector Casper/Assets/Scripts/ActivateCheckpoint.cs
@@ -1,21 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class ActivateCheckpoint : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
-    public Sprite activeSprite;
-    public Sprite unActiveSprite;
+    private Animator animator;
+    public Light2D torch;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        torch.enabled = false;
+    }
 
     public void Activate()
     {
-        spriteRenderer.sprite = activeSprite;
+        animator.SetBool("Active", true);
+        torch.enabled = true;
     }
 
     public void Deactivate()
     {
-        spriteRenderer.sprite = unActiveSprite;
+        animator.SetBool("Active", false);
+        torch.enabled = false;
     }
     
     private void OnTriggerEnter2D(Collider2D other)
