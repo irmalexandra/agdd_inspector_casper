25fda90c25ab2bbb6a0d7d9efe6aee64234449eb
Loki Alexander Hopkins <lokih@ru.is>
2021-03-21 16:20:59 +0000

    Added death sound, fixed reset too soon bug.

--- Inspector Casper/Assets/Scripts/SoundManager.cs
+++ Inspector Casper/Assets/Scripts/SoundManager.cs
@@ -7,13 +7,14 @@ public class SoundManager : MonoBehaviour
 {
     private static AudioClip _shutterSound;
     private static AudioClip _shutterSoundEcho;
+    private static AudioClip _deathSound;
 
     private static AudioSource _audioSource;
     void Start()
     {
         _shutterSound = Resources.Load<AudioClip>("Audio/Sounds/CameraShutter1");
         _shutterSoundEcho = Resources.Load<AudioClip>("Audio/Sounds/CameraShutter1Echo");
-        Debug.Log(_shutterSound);
+        _deathSound = Resources.Load<AudioClip>("Audio/Sounds/Death1");
         _audioSource = GetComponent<AudioSource>();
     }
     
@@ -27,6 +28,10 @@ public class SoundManager : MonoBehaviour
             case "ShutterEcho":
                 _audioSource.PlayOneShot(_shutterSoundEcho);
                 break;
+            case "Death":
+                _audioSource.PlayOneShot(_deathSound);
+                break;
+            
         }
     }
 }
