e483b0a25e08887caec49a8d030ee83496c436cc
Loki Alexander Hopkins <lokih@ru.is>
2021-03-27 19:13:17 +0000

    Thing

--- Inspector Casper/Assets/Scripts/GameManager.cs
+++ Inspector Casper/Assets/Scripts/GameManager.cs
@@ -12,18 +12,18 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject deathCanvas;
-    
+
     public GameObject player;
     public GameObject[] enemies;
-    
+
     private Vector3 _checkPointPosition;
-    
+
     private SpriteRenderer _playerSpriteRenderer;
     private PlayerController _playerController;
     private PlayerMovement _playerMovement;
     private Rigidbody2D _playerRigidBody;
-    
-    private  bool _isDead = false;
+
+    private bool _isDead = false;
 
 
     // Start is called before the first frame update
@@ -33,14 +33,14 @@ public class GameManager : MonoBehaviour
         instance = this;
         _checkPointPosition = player.transform.position;
     }
-    
+
 
     void Start()
     {
         Physics2D.IgnoreLayerCollision(6, 7); // Ceiling check layer and Enemy layer
         Physics2D.IgnoreLayerCollision(9, 7); // Grid layer and Enemy layer
         Physics2D.IgnoreLayerCollision(10, 10); // Ignore self layer and Ignore self layer
-        Physics2D.IgnoreLayerCollision(7,10); // Enemy layer and Ignore self layer
+        Physics2D.IgnoreLayerCollision(7, 10); // Enemy layer and Ignore self layer
         Physics2D.IgnoreLayerCollision(0, 7); // Default layer and Enemy layer
 
         _playerRigidBody = player.gameObject.GetComponent<Rigidbody2D>();
@@ -49,6 +49,14 @@ public class GameManager : MonoBehaviour
         _playerMovement = player.gameObject.GetComponent<PlayerMovement>();
 
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    void flipGhostType()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            
+        }
     }
 
     public PlayerController getPlayerController()
